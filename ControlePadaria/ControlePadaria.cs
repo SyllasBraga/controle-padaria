298431001742992881828917dd4ed3c6444f70f8
Syllas Braga <syllasbraga2@gmail.com>
2023-11-21 20:13:33 -0300

    feat: Initials Structs

---
+++ ControlePadaria/ControlePadaria.cs
@@ -0,0 +1,42 @@
+﻿class ControlePadaria
+{
+    static void Main(string[] args)
+    {
+
+    }
+
+    [Serializable]
+    struct Product{
+        string batch;
+        string description;
+        DateTime validity;
+        float price;
+    }
+
+    [Serializable]
+    struct Stock
+    {
+        Product product;
+        int stockAmount;
+        int stockSaled;
+        int stockInitial;
+    }
+
+    [Serializable]
+    struct Sale 
+    { 
+        Product product;
+        DateTime saleDate;
+        int quantity;
+    }
+
+    [Serializable]
+    struct cashRegister
+    {
+        Sale[] sales;
+        float totalValue;
+        string paymentType;
+        DateTime date;
+    }
+
+}
