ee9a32b0a0a189daf857a1914bf97fcabc3a0120
Syllas Braga <syllasbraga2@gmail.com>
2023-11-24 16:13:41 -0300

    feat: Show products and read bin file of products

--- ControlePadaria/ControlePadaria.cs
+++ ControlePadaria/ControlePadaria.cs
@@ -15,42 +15,74 @@ class ControlePadaria
 
     static void Main(string[] args)
     {
+        showProducts();
         saveProducts();
+        showProducts();
     }
 
     static void saveProducts()
     {
         Console.WriteLine("Quantos produtos deseja cadastrar? " + "\n Limite de " + (products.Length - countProduct));
         int amountProducts = int.Parse(Console.ReadLine());
+        int total = amountProducts + countProduct;
+        Console.Clear();
         for (int i = countProduct; i < amountProducts + countProduct; i++)
         {
             Console.WriteLine("Qual é o lote do produto?");
             products[i].batch = Console.ReadLine();
             Console.WriteLine("Insira uma descrição para o produto: ");
             products[i].description = Console.ReadLine();
-            Console.WriteLine("Qual é a data de validade do produto? \n Digite no formato AAMMYYY. \n Ao digitar o dia, tecle enter. Após o mês, tecle enter. Por fim, após o ano, tecle enter novamente.");
+            Console.WriteLine("Qual é a data de validade do produto? \nDigite no formato AA/MM/YYY.");
+            Console.Write("Dia: ");
             int day = int.Parse(Console.ReadLine());
+            Console.Write("Mês: ");
             int month = int.Parse(Console.ReadLine());
+            Console.Write("Ano: ");
             int year = int.Parse(Console.ReadLine());
             products[i].validity = new DateTime(year, month, day);
             Console.WriteLine("Digite o preço do produto: ");
             products[i].price = float.Parse(Console.ReadLine());
+            Console.Clear();
         }
-        saveProductsBinFile(products);
+        saveProductsBinFile(products, total);
     }
 
-    static void saveProductsBinFile(Product[] productsArray)
+    static void saveProductsBinFile(Product[] productsArray, int total)
     {
         IFormatter formatter = new BinaryFormatter();
         Stream wr = new FileStream("products.bin", FileMode.Create, FileAccess.Write);
 
-        for (int i = 0; i < countProduct; i++)
+        for (int i = 0; i < total; i++)
         {
             formatter.Serialize(wr, productsArray[i]); 
         }
         wr.Close();
     }
-  
+    static void readBinProducts()
+    {
+        IFormatter formatter = new BinaryFormatter();
+        if (File.Exists("products.bin"))
+        {
+            Stream rd = new FileStream("products.bin", FileMode.Open, FileAccess.Read);
+            int i = 0;
+            while (rd.Position < rd.Length)
+            {
+                Product product = (Product)formatter.Deserialize(rd);
+                products[i] = product;
+                i++;
+            }
+            rd.Close();
+        }
+    }
+    static void showProducts()
+    {
+        Array.Clear(products, 0, products.Length);
+        readBinProducts();
+        for (int i = 0; i<products.Length; i++)
+        {
+            Console.WriteLine(products[i].ToString());
+        }
+    }
 
     [Serializable]
     struct Product{
@@ -58,6 +90,8 @@ class ControlePadaria
         public string description;
         public DateTime validity;
         public float price;
+        public override string ToString() =>
+            $"Lote: {batch}; Descrição: {description}; Validade: {validity}; Preço: {price}";
     }
 
     [Serializable]
